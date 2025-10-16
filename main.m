d4a1574f8969fef49058f5816973397cbdaef0b5
mirachew <mira@thechewfamily.com>
2025-10-15 22:30:46 -0700

    updated main

--- main.m
+++ main.m
@@ -60,13 +60,15 @@ title('Function 02 y-values')
 
 t_ref =  0.492;
 
+filter_params = struct();
+filter_params.max_xval = 0.1;
 [h_list_01, analytical_difference_01, e_local_FE_01, e_local_MP_01] = local_truncation_error(@rate_func01, @solution01, t_ref);
-[p_FE_01,k_FE_01] = loglog_fit(h_list_01,e_local_FE_01)
-[p_MP_01,k_MP_01] = loglog_fit(h_list_01,e_local_MP_01)
+[p_FE_01,k_FE_01] = loglog_fit(h_list_01,e_local_FE_01, filter_params)
+[p_MP_01,k_MP_01] = loglog_fit(h_list_01,e_local_MP_01, filter_params)
 
 [h_list_02, analytical_difference_02, e_local_FE_02, e_local_MP_02] = local_truncation_error(@rate_func02, @solution02, t_ref);
-[p_FE_02,k_FE_02] = loglog_fit(h_list_02,e_local_FE_02)
-[p_MP_02,k_MP_02] = loglog_fit(h_list_02,e_local_MP_02)
+[p_FE_02,k_FE_02] = loglog_fit(h_list_02,e_local_FE_02, filter_params)
+[p_MP_02,k_MP_02] = loglog_fit(h_list_02,e_local_MP_02, filter_params)
 
 figure(3)
 loglog(h_list_01,analytical_difference_01, 'DisplayName', 'Analytical Difference');
@@ -90,6 +92,8 @@ legend
 
  
 % % ------------- Global Truncation Error --------------------------
+
+% Implementing for Function 01
 t0 = 0;
 tf = 10;
 t_span = [t0, tf];
@@ -98,8 +102,14 @@ t_span = [t0, tf];
 
 filter_params = struct();
 filter_params.max_xval = .1;
-[p_FE_01,k_FE_01] = loglog_fit(h_list_01,e_global_FE_01,filter_params);
-[p_MP_01,k_MP_01] = loglog_fit(h_list_01,e_global_MP_01,filter_params);
+[p_FE_01,k_FE_01] = loglog_fit(h_list_01,e_global_FE_01,filter_params)
+[p_MP_01,k_MP_01] = loglog_fit(h_list_01,e_global_MP_01,filter_params)
+
+filter_params = struct();
+filter_params.min_xval = 80;
+[p_numevals_FE_01,k_numevals_FE_01] = loglog_fit(num_evals_FE_01,e_global_FE_01,filter_params)
+[p_numevals_MP_01,k_numevals_MP_01] = loglog_fit(num_evals_MP_01,e_global_MP_01,filter_params)
+
 
 
 figure()
@@ -116,11 +126,51 @@ figure()
 loglog(num_evals_FE_01, e_global_FE_01, 'DisplayName', 'Forward Eurler')
 hold on
 loglog(num_evals_MP_01, e_global_MP_01, 'DisplayName', 'Explicit Midpoint')
+loglog(num_evals_FE_01, k_numevals_FE_01*num_evals_FE_01.^p_numevals_FE_01, 'DisplayName', 'FE linear')
+loglog(num_evals_MP_01, k_numevals_MP_01*num_evals_MP_01.^p_numevals_MP_01, 'DisplayName', 'MP linear')
+title('# Evaluations vs. Global Error Func 01')
+xlabel('# Evaluations')
+ylabel('Global Error')
+legend()
 
-%-------------------- Backwards Euler--------------------------------
+
+% Implementing for Function 02
+
+t0 = 0;
+tf = 10;
+t_span_02 = [t0, tf];
+
+[h_list_02, e_global_FE_02, e_global_MP_02, num_evals_FE_02, num_evals_MP_02] = global_truncation_error(@rate_func02, @solution02, t_span_02);
+
+filter_params = struct();
+filter_params.max_xval = .1;
+[p_FE_02,k_FE_02] = loglog_fit(h_list_02,e_global_FE_02,filter_params)
+[p_MP_02,k_MP_02] = loglog_fit(h_list_02,e_global_MP_02,filter_params)
+
+filter_params = struct();
+filter_params.min_xval = 80;
+[p_numevals_FE_02,k_numevals_FE_02] = loglog_fit(num_evals_FE_02,e_global_FE_02,filter_params)
+[p_numevals_MP_02,k_numevals_MP_02] = loglog_fit(num_evals_MP_02,e_global_MP_02,filter_params)
 
 
 
+figure()
+loglog(h_list_02, e_global_FE_02, 'DisplayName', 'Forward Euler Error')
+hold on
+loglog(h_list_02, e_global_MP_02, 'DisplayName', 'Explicit Midpoint Error')
+loglog(h_list_02, k_FE_02*h_list_02.^p_FE_02, 'DisplayName', 'FE linear')
+loglog(h_list_02, k_MP_02*h_list_02.^p_MP_02, 'DisplayName', 'MP linear')
+title("Global Truncation Error Function 02")
+legend()
 
 
-
+figure()
+loglog(num_evals_FE_02, e_global_FE_02, 'DisplayName', 'Forward Eurler')
+hold on
+loglog(num_evals_MP_02, e_global_MP_02, 'DisplayName', 'Explicit Midpoint')
+loglog(num_evals_FE_02, k_numevals_FE_02*num_evals_FE_02.^p_numevals_FE_02, 'DisplayName', 'FE linear')
+loglog(num_evals_MP_02, k_numevals_MP_02*num_evals_MP_02.^p_numevals_MP_02, 'DisplayName', 'MP linear')
+title('# Evaluations vs. Global Error Func 02')
+xlabel('# Evaluations')
+ylabel('Global Error')
+legend()
